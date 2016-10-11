18fe4a7428b586c4609423e2d11a4f2efda4a968
Ricky Valencia <3d.rickyV@gmail.com>
2016-10-11 11:27:30 -0700

    change url to execute only in class listing

--- bootstrap.js
+++ bootstrap.js
@@ -1,6 +1,7 @@
-chrome.tabs.onUpdated.addListener(function(id, info, tab) {
-  if (tab.url.toLocaleLowerCase().indexOf("banweb.pdx.edu") > -1 && info.status === 'loading') {
+chrome.tabs.onUpdated.addListener(function (id, info, tab) {
+  if (tab.url.toLocaleLowerCase().indexOf("https://banweb.pdx.edu/pls/oprd/bwskfcls.p_getcrse") > -1
+      && info.status === 'loading') {
     chrome.pageAction.show(tab.id);
-    chrome.tabs.executeScript(tab.id, {"file": "extension.js"});
+    chrome.tabs.executeScript(tab.id, { "file": "extension.js" });
   }
 });
