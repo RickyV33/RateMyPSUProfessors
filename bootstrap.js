b17bf755f61ee7ff200d7a210f0051d86aabb377
Ricky Valencia <3d.rickyV@gmail.com>
2016-10-11 01:04:27 -0700

    add script tab id and status checking

--- bootstrap.js
+++ bootstrap.js
@@ -1,6 +1,6 @@
 chrome.tabs.onUpdated.addListener(function(id, info, tab) {
-  if (tab.url.toLocaleLowerCase().indexOf("banweb.pdx.edu") > -1) {
+  if (tab.url.toLocaleLowerCase().indexOf("banweb.pdx.edu") > -1 && info.status === 'loading') {
     chrome.pageAction.show(tab.id);
-    chrome.tabs.executeScript(null, {"file": "extension.js"});
+    chrome.tabs.executeScript(tab.id, {"file": "extension.js"});
   }
 });
