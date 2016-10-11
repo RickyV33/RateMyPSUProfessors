e0528b98053515ec702c0660d2aadd10c0975b82
Ricky Valencia <3d.rickyV@gmail.com>
2016-10-11 13:31:11 -0700

    remove bootstrap.js

--- bootstrap.js
+++
@@ -1,7 +0,0 @@
-chrome.tabs.onUpdated.addListener(function (id, info, tab) {
-  if (tab.url.toLocaleLowerCase().indexOf("https://banweb.pdx.edu/pls/oprd/bwskfcls.p_getcrse") > -1
-      && info.status === 'loading') {
-    chrome.pageAction.show(tab.id);
-    chrome.tabs.executeScript(tab.id, { "file": "extension.js" });
-  }
-});
