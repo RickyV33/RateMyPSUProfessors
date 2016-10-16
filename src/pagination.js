883f26e00d84f93fe65f218a9302db29187111a8
Ricky Valencia <3d.rickyV@gmail.com>
2016-10-15 19:34:23 -0700

    fix recursive error, add contains professor listing check

--- src/pagination.js
+++ src/pagination.js
@@ -1,28 +1,40 @@
 import request from 'browser-request' ;
 
 
-export default function pagination(url, startAt, maxRestults, step = 20) {
+export default function pagination (url, startAt = 0,
+                                    maxRestults = NUMBER.MAX_SAFE_INTEGER,
+                                    step = 20) {
   return new Promise((resolve, reject) => {
     let results = [];
-    function getPaginatedData(min, max) {
-      if (min >= max) {
-        resolve(data);
+
+    function getPaginatedData (min, max) {
+      if (containsProfessorListing(results) || min >= max)  {
+        console.log('HIT');
+        resolve(results);
+      } else {
+        request({
+          url: url,
+          qs: { 'offset': min },
+          headers: { 'Content-Type': 'text/html' },
+          method: 'GET'
+        }, (error, response, body) => {
+          if (error) {
+            reject(error);
+          }
+          results.push(body);
+          getPaginatedData(min + step, max);
+        });
       }
-      request({
-        url: url,
-        qs: { 'offset': min },
-        headers: {
-          'Content-Type': 'text/html',
-        },
-        method: 'GET'
-      }, (error, response, body) => {
-        if (error) {
-          reject(error);
-        }
-        results.push(body);
-        getPaginatedData(min + step, max);
-      });
     }
+
     getPaginatedData(startAt, maxRestults)
   });
+}
+
+
+function containsProfessorListing (data) {
+  let lastEntry = document.createTextNode(data.splice(-1)[0]);
+  let DOM = document.createElement('div');
+  DOM.appendChild(lastEntry);
+  return DOM.querySelector('.listing .PROFESSOR');
 }
