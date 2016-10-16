8dcfc587b6437074a377e2d4c5153bf282e65755
Ricky Valencia <3d.rickyV@gmail.com>
2016-10-15 18:33:53 -0700

    implement pagination function recursively

--- src/pagination.js
+++ src/pagination.js
@@ -1,18 +1,28 @@
-'use strict';
-import request from 'request';
+import request from 'browser-request' ;
 
 
-default export function pagination(url, maxRestults) {
+export default function pagination(url, startAt, maxRestults, step = 20) {
   return new Promise((resolve, reject) => {
-    request({
-      url: url,
-      qs: {'offset': maxRestults},
-      headers: {
-        'Content-Type': 'text/html',
-      },
-      method: 'GET'
-    }, (error, response, body) => {
-      console.log(body);
-    });
+    let results = [];
+    function getPaginatedData(min, max) {
+      if (min >= max) {
+        resolve(data);
+      }
+      request({
+        url: url,
+        qs: { 'offset': min },
+        headers: {
+          'Content-Type': 'text/html',
+        },
+        method: 'GET'
+      }, (error, response, body) => {
+        if (error) {
+          reject(error);
+        }
+        results.push(body);
+        getPaginatedData(min + step, max);
+      });
+    }
+    getPaginatedData(startAt, maxRestults)
   });
 }
