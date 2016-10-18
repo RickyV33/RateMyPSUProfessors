c0003930cbfc4300cf5aad32fd0197168e9e59f9
Ricky Valencia <3d.rickyV@gmail.com>
2016-10-18 00:10:48 -0700

    recursion works

--- src/pagination.js
+++ src/pagination.js
@@ -1,63 +1,51 @@
-import request from 'browser-request' ;
+import request from 'browser-request';
 
 
 export default function pagination (url, startAt = 0,
                                     maxRestults = Number.MAX_SAFE_INTEGER,
                                     step = 20) {
-  let parser = new DOMParser();
-  let DOM;
-
   return new Promise((resolve, reject) => {
     let results = [];
+    let DOM;
+    let professors;
+    let parser = new DOMParser();
 
     function getPaginatedData (min, max) {
-      if (containsProfessorListing(results) || min >= max) {
-        resolve(results);
-      } else {
-        request({
-          url: url,
-          qs: { 'offset': min },
-          headers: { 'Content-Type': 'text/html' },
-          method: 'GET'
-        }, (error, response, body) => {
-          if (error) {
-            reject(error);
-          }
-          DOM = parser.parseFromString(body, "text/html");
-          let foo = Array.from(DOM.querySelectorAll('.PROFESSOR'));
-          results.push(...foo);
-          return getPaginatedData(min + step, max);
-        });
-      }
+      request({
+        url: url,
+        qs: { 'offset': min },
+        headers: { 'Content-Type': 'text/html' },
+        method: 'GET'
+      }, (error, response, body) => {
+        if (error) {
+          reject(error);
+        }
+        DOM = parser.parseFromString(body, "text/html");
+        professors = parseProfessorInfo(Array.from(DOM.getElementsByClassName('PROFESSOR')));
+        results.push(...professors);
+        min += step;
+        if (professors.length === 0 || min >= max) {
+          resolve(results);
+        } else {
+          return getPaginatedData(min, max);
+        }
+      });
     }
 
     getPaginatedData(startAt, maxRestults)
   });
 }
 
-function parseProfessorInfo(listing) {
-  let info = {};
-  //listing.
-  // Parse the listing HTML and insert it into info listing looks like such:
-  /*
-   <li class="listing PROFESSOR">
-     <a href="/ShowRatings.jsp?tid=132256">
-       <span class="listing-cat">
-         <span class="icon icon-professor"></span>
-         PROFESSOR
-       </span>
-       <span class="listing-name">
-         <span class="main">Fant, Karla </span>
-         <span class="sub">Portland State University, Computer Science</span>
-       </span>
-     </a>
-   </li>
-   */
-
-}
-
-function containsProfessorListing (HTMLString) {
-  let parser = new DOMParser();
-  let DOM = parser.parseFromString(HTMLString, "text/html");
-  return DOM.querySelector('.listing .PROFESSOR');
+function parseProfessorInfo(listings) {
+  let info;
+  let anchor;
+  if (!listings) return [];
+  return listings.map(listing => {
+    info = {};
+    anchor = listing.firstElementChild;
+    info.link = 'www.ratemyprofessor.com' + anchor.getAttribute('href');
+    [ info.last, info.first ] = anchor.getElementsByClassName('main')[0]
+      .innerText.split(', ');
+    return info;
+  });
 }
