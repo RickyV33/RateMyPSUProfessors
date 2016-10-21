4a2d901d4ec0b39c5cda86b12d04ca44bda2a72b
Ricky Valencia <3d.rickyV@gmail.com>
2016-10-21 16:34:08 -0700

    refactoring to use server

--- src/extension.js
+++ src/extension.js
@@ -1,72 +1,114 @@
-import pagination from './pagination';
-
-const RMP_URL = `https://www.ratemyprofessors.com/search.jsp?queryoption=HEADER&
-queryBy=teacherName&schoolName=Portland+State+University&schoolID=&query={name}`;
+import { getProfessorLinks, getProfessorInfo } from './pagination';
+import request from 'browser-request';
+import { uniq } from 'lodash';
 
 let tableBody = document.querySelector('.datadisplaytable tbody');
 
 function getTeacherNames () {
   let teacherNameList = Array.from(tableBody.querySelectorAll('tr:nth-child(n+4) td:nth-child(18)'));
 
-  return teacherNameList.map(name => {
-    let isPrimaryTeacherIndex;
-    let teacherName = name.innerText;
-    isPrimaryTeacherIndex = teacherName.indexOf('(');
+  return Promise.resolve(uniq(teacherNameList.map(name => {
+    return name.innerText;
+  })).map(name => {
+    name = name.split(' ');
+    // Remove the (P) tag from the name
+    let isPrimaryTeacherIndex = name.indexOf('(P)');
     if (isPrimaryTeacherIndex > -1) {
-      return teacherName.slice(0, isPrimaryTeacherIndex - 1);
+      name.pop();
     }
-    return teacherName;
-  });
+    // Returns only the first and last name - excluding the middle (if it
+    // exists)
+    let first = name.shift();
+    let last = name.pop();
+    return {
+      first: first,
+      last: last
+    };
+  }));
 }
 
-function updateHeaderColumn() {
+function updateHeaderColumn () {
   let rateMyProfessorColumn = document.createElement('th');
   let ratingText = document.createTextNode('Rate My Professor');
-  rateMyProfessorColumn.appendChild(ratingText);
-  rateMyProfessorColumn.classList.add('ddheader');
   let headers = tableBody.querySelector('tr:nth-child(3)');
   let teacherColumn = headers.querySelector('th:nth-child(20)');
+  rateMyProfessorColumn.appendChild(ratingText);
+  rateMyProfessorColumn.classList.add('ddheader');
   headers.insertBefore(rateMyProfessorColumn, teacherColumn);
 }
 
-function updateContentColumns() {
+function updateContentColumns (teacher) {
   let contentRows = Array.from(tableBody.querySelectorAll('tr:nth-child(n+4)'));
-  contentRows.forEach(row => {
-    let column = document.createElement('td');
-    column.setAttribute('style', 'font-size: .75em');
-    column.classList.add('dddefault');
-    let quality = insertIntoDiv(document.createTextNode('Quality: 4.0'));
-    let difficulty = insertIntoDiv(document.createTextNode('Difficulty: 3.3'));
+
+  contentRows.forEach((row, i) => {
+    let contentColumn = document.createElement('td');
+    let quality = insertIntoDiv(document.createTextNode(teacher[i].quality));
+    let easiness = insertIntoDiv(document.createTextNode(teacher[i].easiness));
     let link = document.createElement('a');
-    link.setAttribute('href', 'www.google.com');
+    contentColumn.setAttribute('style', 'font-size: .75em');
+    contentColumn.classList.add('dddefault');
+    link.setAttribute('href', teacher[i].url);
     link.appendChild(document.createTextNode('More info...'));
     link = insertIntoDiv(link);
-    addChildren(column, quality, difficulty, link);
-    row.insertBefore(column, row.querySelector('td:nth-child(19)'));
+    addChildren(contentColumn, quality, easiness, link);
+    row.insertBefore(contentColumn, row.querySelector('td:nth-child(19)'));
   });
 }
 
-function insertIntoDiv(element) {
+function insertIntoDiv (element) {
   let div = document.createElement('div');
   div.appendChild(element);
   return div;
 }
 
-function addChildren(parent, ...children) {
+function addChildren (parent, ...children) {
   children.forEach(child => {
     parent.appendChild(child);
   });
 }
-updateHeaderColumn();
-updateContentColumns();
-let name = 'Karla+Fant';
-let prof = RMP_URL.replace('{name}', name);
-pagination(prof).then(results => {
-  results.forEach((body, index) => {
-    console.log('&&&&&&&&&&&&&&&');
-    console.log(index);
-    console.log(body)
-  })
-}, error => {
-  console.error('oops! There was an error: ' + error);
-});
+
+function onInit () {
+  updateHeaderColumn();
+  getTeacherNames().then(names => {
+    return getRateMyProfessorInfo(names)
+  }).then(teacherInfo => {
+    console.log(teacherInfo);
+  });
+}
+
+function getRateMyProfessorInfo (teacherNames) {
+  let info = {};
+
+  return new Promise((resolve, reject) => {
+    function getTeacherInfo (names) {
+      if (names.length === 0) {
+        resolve(info);
+      } else {
+        let name = names.pop();
+        request({
+          url: 'https://cortana-center.herokuapp.com/RateMyProfessor',
+          qs: {
+            first: name.first,
+            last: name.last
+          },
+          headers: { 'Content-Type': 'text/html' },
+          method: 'GET'
+        }, (error, response, body) => {
+          if (error) {
+            reject(error);
+          }
+          info = Object.assign(info, body);
+          // info = {...info, ...body};
+          getTeacherInfo(names);
+        });
+      }
+    }
+
+    getTeacherInfo(teacherNames);
+  });
+}
+
+// fires when script is first loaded
+// can't do onInit directly here, because the DOM hasn't been loaded yet
+// we just set an event listener for document.DOMContentLoaded
+document.addEventListener('DOMContentLoaded', onInit(), false);
--- src/pagination.js
+++ src/pagination.js
@@ -1,65 +1,62 @@
-import request from 'browser-request';
-
-function getProfessorLinks (url) {
-  return new Promise((resolve, request) => {
-    let results = [];
-    let DOM;
-    let professorLink;
-    let parser = new DOMParser();
-
-    request({
-      url: url,
-      headers: { 'Content-Type': 'text/html' },
-      method: 'GET'
-    }, (error, response, body) => {
-      if (error) {
-        reject(error);
-      }
-      DOM = parser.parseFromString(body, "text/html");
-      professorLink = parseProfessorLink(Array.from(DOM.getElementsByClassName('PROFESSOR')));
-      results.push(...professorLink);
-      resolve(results);
-    });
-  });
-}
-
-function getProfessorInfo (url) {
-  return new Promise((resolve, reject) => {
-    let results = [];
-    let parser = new DOMParser();
-
-    request({
-      url: url,
-      headers: { 'Content-Type': 'text/html' },
-      method: 'GET'
-    }, (error, response, body) => {
-      if (error) {
-        reject(error);
-      }
-      results.push(...parseProfessorInfo(parser.parseFromString(body, "text/html")));
-      resolve(results);
-    });
-  });
-
-}
-
-function parseProfessorLink (listings) {
-  if (!listings) {
-    return null;
-  }
-  return listings.map(listing => {
-    return 'www.ratemyprofessor.com' +
-           listing.firstElementChild.getAttribute('href');
-  });
-}
-
-function parseProfessorInfo (DOM) {
-  let ratingsDiv = Array.from(DOM.getElementsByClassName('breakdown-wrapper'));
-  if (!ratingsDiv) {
-    return null;
-  }
-  let info = {};
-  [ info.first, , info.last ] = Array.from(DOM.querySelector('h1 .profname').childNodes);
-  [info.quality, info.difficulty, ,] = Array.from(DOM.getElementsByClassName('grade'));
-  return info;
-}
+// import request from 'browser-request';
+//
+// export function getProfessorLinks (url) {
+//   return new Promise((resolve, reject) => {
+//     let parser = new DOMParser();
+//
+//     request({
+//       url: url,
+//       headers: { 'Content-Type': 'text/html' },
+//       method: 'GET'
+//     }, (error, response, body) => {
+//       if (error) {
+//         reject(error);
+//       }
+//       resolve(parseProfessorLink(parser.parseFromString(body, 'text/html')));
+//     });
+//   });
+// }
+//
+// function parseProfessorLink (DOM) {
+//   let professors = Array.from(DOM.getElementsByClassName('PROFESSOR'));
+//   if (!professors) {
+//     return null;
+//   }
+//   //return 'https://us9.proxysite.com/process.php?d=joV9rFmPUEpTk87STEfBgcJDJGgPmHg8rcq6jxmDs%2BWUFnGpZm4pCWBf%2B0%2F1xO7QTAulyXlq&b=1&f=norefer';
+//   return professors.map(listing => {
+//     console.log('----->' + listing.firstElementChild.getAttribute('href'));
+//     return 'https://www.ratemyprofessor.com' +
+//            listing.firstElementChild.getAttribute('href');
+//   }).slice(-1).pop();
+// }
+//
+// export function getProfessorInfo (url) {
+//   return new Promise((resolve, reject) => {
+//     document.domain = 'ratemyprofessor.com';
+//     let parser = new DOMParser();
+//     request({
+//       url: url,
+//       headers: { 'Content-Type': 'text/html', 'Origin': 'https://banweb.pdx.edu/pls/oprd/bwskfcls.P_GetCrse' },
+//       method: 'GET'
+//     }, (error, response, body) => {
+//       if (error) {
+//         reject(error);
+//       }
+//       console.log(body);
+//       resolve(parseProfessorInfo(parser.parseFromString(body, "text/html")));
+//     });
+//   });
+//
+// }
+//
+// function parseProfessorInfo (DOM) {
+//   let ratingsDiv = Array.from(DOM. getElementsByClassName('breakdown-wrapper'));
+//
+//   if (!ratingsDiv) {
+//     return null;
+//   }
+//   let info = {};
+//   [info.first, , info.last] = Array.from(DOM.querySelector('h1.profname').childNodes);
+//   [info.quality, info.difficulty, ,] = Array.from(DOM.getElementsByClassName('grade'));
+//   return info;
+// }
