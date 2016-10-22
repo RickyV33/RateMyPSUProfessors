9b00eb6aceca48851d5d3f43a253e38f9d302892
Ricky Valencia <3d.rickyV@gmail.com>
2016-10-21 18:35:02 -0700

    fix background to run properly and use callback

--- src/background.js
+++ src/background.js
@@ -1,17 +1,25 @@
 import request from 'browser-request';
 
-chrome.runtime.onMessage.addListener(function(req, sender, callback) {
-  if (req.action === 'getTeacherLinks') {
+chrome.runtime.onMessage.addListener(function (req, sender, callback) {
+  if (req.action === 'getProfessorLink') {
+    console.log('hi');
     request(req.options, (error, response, body) => {
+      error = true;
       if (error) {
-        callback({error: error, response: reponse, body: body});
+        callback({
+          error: error,
+          response: response
+        });
       }
       callback(body);
     });
-  } else if (req.action === 'getTeacherInfo') {
+  } else if (req.action === 'getProfessorInfo') {
     request(req.options, (error, response, body) => {
       if (error) {
-        callback({error: error, response: reponse, body: body});
+        callback({
+          error: error,
+          response: response
+        });
       }
       callback(body);
     });
